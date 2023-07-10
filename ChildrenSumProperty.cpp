8f202e893f812c2a7bcf25747fce6e514976c609
Pawan Meena <72181228+pawan09032004@users.noreply.github.com>
2023-07-11 01:16:14 +0530

    Create ChildrenSumProperty.cpp

---
+++ ChildrenSumProperty.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    // Write your code here.
+    if(root==NULL)return;
+    int child=0;
+    if(root->left!=NULL)child+=root->left->data;
+    if(root->right!=NULL)child+=root->right->data;
+
+    if(child>=root->data)root->data=child;
+    else{
+        if(root->left!=NULL)root->left->data=root->data;
+        if(root->right!=NULL)root->right->data=root->data;
+    }
+
+    changeTree(root->left);
+    changeTree(root->right);
+
+    int total=0;
+    if(root->left!=NULL)total+=root->left->data;
+    if(root->right!=NULL)total+=root->right->data;
+    if(root->left || root->right)root->data=total;
+
+}   
